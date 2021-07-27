bf1bf17ed986e34d489846bb921f7783b16f22fe
liuxiankun <liuxiankun@zhangyue.com>
2021-07-27 12:11:22 +0800

    feat:新增vue项目

---
+++ src/components/HelloWordInput.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        user: {
+            type: String,
+            required: true
+        }
+    },
+    data () {
+        return {
+            innerObj: {
+                a: 12
+            },
+            initName: this.user,
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    methods: {
+        changeInitName() {
+            this.$emit('update:user', this.initName)
+        },
+        handleClickParent() {
+            // console.log('handleClickParent')
+        },
+        handleClickSon(e) {
+            console.log(this)
+        },
+        changeInputValue(e) {
+            this.$emit('input', e.target.value)
+        }
+    },
+    render() {
+        const { name } = this;
+        console.log('this.$slots.default-------->', this.$slots.default)
+        return (
+            <div>   
+                {this.$slots.default || <slot v-bind:innerObj="innerObj">默认</slot>}
+            </div>
+        )
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+    <hello-world-input >
+        {{ msg }}
+    </hello-world-input>
+</template>
+<script>
+import HelloWorldInput from './HelloWordInput.vue'
+export default {
+    name: 'HelloWorld',
+    props: {
+        name: {
+            type: String,
+            default: '123'
+        }
+    },
+    data () {
+        return {
+            user: {
+                name: '123'
+            },
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    methods: {
+        handleClickParent() {
+            // console.log('handleClickParent')
+        },
+        handleClickSon(e) {
+            console.log(this)
+        },
+        changeInputValue(v) {
+            console.log(v, this.user.name)
+        }
+    },
+    watch: {
+        msg(value) {
+            console.log(1111111111, value)
+        }
+    },
+    components: {
+        HelloWorldInput
+    },  
+    render() {
+        const {
+            user
+        } = this;
+        return (
+            <div>
+                <HelloWorldInput 
+                    user={user}
+                    on-input={this.changeInputValue}
+                />
+            </div>
+        )
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.parent{
+    width: 100px;
+    height: 100px;
+    background-color: red;
+}
+.son{
+    width: 50px;
+    height: 50px;
+    background-color: pink;
+}
+</style>
